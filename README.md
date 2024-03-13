72c44dd25f4dd404de767819171a119caa7f0426
ankit2005-star <ankitgodbole90@gmail.com>
2024-03-13 21:57:57 +0530

    created calculator hsj

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-code
-This the repository where I put my work.
--- WebWorker/add.js
+++
@@ -1,4 +0,0 @@
- this.onmessage = function(e){
-    const{data} = e;
-    this.addThread.postMessage({"addresult":data.arg1 + data.arg2});
- }
